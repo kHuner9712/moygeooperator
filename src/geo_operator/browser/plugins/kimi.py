b1f4a76aea25e67365114bd2072f3881c63446da
盛鑫 <shengxin9712@gmail.com>
2026-08-23 23:18:18 +0800

    fix: refresh Kimi composer and stop state selectors

---
+++ src/geo_operator/browser/plugins/kimi.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from geo_operator.browser.plugins.additional import KimiPlugin as _AdditionalKimiPlugin
+from geo_operator.browser.plugins.phase1 import PhaseOneSelectors
+
+
+class KimiPlugin(_AdditionalKimiPlugin):
+    """Kimi adapter refreshed for the current composer state model.
+
+    Kimi uses the same send-button container as both the idle send control and the in-flight stop
+    control. Current pages mark the in-flight state with the ``stop`` class, so an actionable send
+    selector must explicitly exclude both ``stop`` and the older ``loading`` state. Otherwise the
+    worker can record a QUERY_SEND intent and click a stop control without ever delivering the
+    prompt.
+    """
+
+    observed_at = "2026-08-23"
+
+    selectors = PhaseOneSelectors(
+        login_indicators=_AdditionalKimiPlugin.selectors.login_indicators,
+        prompt_inputs=(
+            "div.chat-input-editor[role='textbox'][contenteditable='true']",
+            "div.chat-input-editor[contenteditable='true']",
+            "div[role='textbox'][contenteditable='true']",
+        ),
+        send_controls=(
+            "div.send-button-container:not(.disabled):not(.stop):not(.loading)",
+            "div[role='button'][aria-label='Send']:not([aria-disabled='true'])",
+            "button[aria-label='Send']:not([disabled])",
+        ),
+        user_queries=_AdditionalKimiPlugin.selectors.user_queries,
+        responses=_AdditionalKimiPlugin.selectors.responses,
+        streaming_indicators=(
+            "div.send-button-container.stop",
+            "div.send-button-container.loading",
+        ),
+        stop_controls=(
+            "div.send-button-container.stop",
+            "div.send-button-container.loading",
+        ),
+        error_indicators=_AdditionalKimiPlugin.selectors.error_indicators,
+        rate_limit_indicators=_AdditionalKimiPlugin.selectors.rate_limit_indicators,
+        security_indicators=_AdditionalKimiPlugin.selectors.security_indicators,
+        conversation_menu_controls=_AdditionalKimiPlugin.selectors.conversation_menu_controls,
+        delete_controls=_AdditionalKimiPlugin.selectors.delete_controls,
+        delete_confirm_controls=_AdditionalKimiPlugin.selectors.delete_confirm_controls,
+        final_response_descendants=_AdditionalKimiPlugin.selectors.final_response_descendants,
+        query_failure_descendants=_AdditionalKimiPlugin.selectors.query_failure_descendants,
+    )
